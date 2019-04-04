6334da5cdfa86091d987b025adb38f92a5afb5f7
Nyarlathotepss <rcp.nmillet@gmail.com>
2019-04-04 14:09:45 +0200

    index.html modified

--- views.py
+++ views.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+app.debug = True
 
 
-@app.route('/')
+@app.route('/index/', methods=['GET', 'POST'])
 def index():
-    return "Helle world!"
+    if request.method == 'GET':
+        #affiche la page et le formulaire
+
+    else:
+        #traiter les infos
+        #afficher le résultat
+
 
 
 if __name__ == "__main__":
