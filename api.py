52c50066f1070e9bba2fd9848aff576d5e449677
Nyarlathotepss <rcp.nmillet@gmail.com>
2019-04-16 20:38:16 +0200

    api.py has been modified >>>> 2 class = 1 class api main.py >>>> class Interaction has been created : user input <> api

--- api.py
+++ api.py
@@ -1,19 +1,24 @@
 import requests
 
 
-class GoogleMapsApi:
-
-    def __init__(self):
-        self.request = None
-        self.PARAMETERS = {}
-
-
-class WikipediaApi:
+class ApiCommunication:
 
     def __init__(self):
         self.r = requests.session()
-        self.WIKI_PARAMETERS = {"action": "query",
-                                "format": "json"}
+        self.PARAMETERS = {}
+
+    def generate_parameters_gmaps(self, input_user):
+        self.PARAMETERS = {"query": input_user,
+                           "key": "AIzaSyAdO62_IhTqYg8IROK76ACbzm-xnbhyC2g"}
+
+    def generate_parameters_wiki(self, input_user):
+        self.PARAMETERS = {"action": "query",
+                           "format": "json",
+                           "text": input_user}
+
+    def get_map(self, url, parameters):
+        self.r = self.r.get(url, parameters)
+        return self.r
 
     def get_info(self, url, parameters):
         self.r = self.r.get(url, parameters)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from constant import stop_words
+from constant import *
+from api import ApiCommunication
 
 
 class ModifyUserInput:
@@ -9,10 +10,32 @@ class ModifyUserInput:
         self.list_user_input = []
 
     def split_text(self, user_input):
+        """string to list"""
         self.user_input = user_input.split()
 
     def clean_text(self, user_input_split):
-        """clean text and transform to list"""
+        """clean text of stop words"""
         for word in user_input_split:
             if word not in stop_words:
                     self.list_user_input.append(word)
+
+
+class Interaction:
+    """action between user and api"""
+
+    def __init__(self):
+        self.wikipedia_return = None
+        self.googlemap_return = None
+        self.communication = ApiCommunication()
+
+    def wiki_comm(self, input_user):
+        """wikipedia return info about user input"""
+        PARAMETERS = self.communication.generate_parameters_wiki(input_user)
+        self.wikipedia_return =  self.communication.get_info(wiki_url, PARAMETERS)
+        return self.wikipedia_return
+
+    def gmap_comm(self, input_user):
+        """google maps return a map about user input"""
+        PARAMETERS = self.communication.generate_parameters_gmaps(input_user)
+        self.googlemap_return = self.communication.get_map(maps_url, PARAMETERS)
+        return self.googlemap_return
