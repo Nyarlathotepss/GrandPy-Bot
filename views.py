e776d66171fb3362b8b3d9b999596b37ce4a5cad
Nyarlathotepss <rcp.nmillet@gmail.com>
2019-04-30 11:12:14 +0200

    update template dialog > variable OK update body >>> return list OK > improve the display

--- views.py
+++ views.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for
 from main import ModifyUserInput
 
 app = Flask(__name__)
 app.debug = True
 interaction = ModifyUserInput()
+la_question = []
 
 
 @app.route('/accueil/', methods=['GET', 'POST'])
@@ -12,18 +13,10 @@ def accueil():
         return render_template('accueil.html')
 
     else:
-        interaction.input_user = request.form["user_question"]
-        interaction.split_text(interaction.input_user)
-        interaction.clean_text(interaction.input_user)
-
-
-@app.route('/dialog/', methods=['GET', 'POST'])
-def dialog():
-    if request.method == 'GET':
-        return render_template('dialog.html')
-
-    else:
-        return render_template('dialog.html')
+        user_question = request.form
+        print(user_question)
+        la_question.append(user_question["question"])
+        return render_template('dialog.html', la_question=la_question)
 
 
 if __name__ == "__main__":
