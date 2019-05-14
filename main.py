f332dd2bafaa62f7ec0ac8c29c35bb7170c24b36
Nyarlathotepss <rcp.nmillet@gmail.com>
2019-05-14 06:30:03 +0200

    main.py > creation format_text_to_search

--- main.py
+++ main.py
@@ -15,20 +15,20 @@ class ModifyUserInput:
         """string to list"""
         self.user_input_split = user_input.split()
 
-    def clean_text(self):
+    def clean_text(self, user_input_split):
         """clean text of stop words"""
-        for word in self.user_input_split:
+        for word in user_input_split:
             if word not in stop_words:
                     self.list_input_user_cleaned.append(word)
 
-    def format_text_to_search(self):
-        for word in self.list_input_user_cleaned:
-            self.input_to_search += word + "|"
+    def format_text_to_search(self, list_input_cleaned):
+        """format text for wikipedia API"""
+        self.input_to_search = "|".join(list_input_cleaned)
 
     def modification_process(self):
         self.user_input.split_text(self.user_input)
-        self.user_input_split.clean_text()
-        self.list_input_user_cleaned.format_text_to_search()
+        self.user_input_split.clean_text(self.user_input_split)
+        self.list_input_user_cleaned.format_text_to_search(self.list_input_user_cleaned)
 
 
 class Interaction:
@@ -46,8 +46,8 @@ class Interaction:
         self.wikipedia_return = self.communication.get_info(wiki_url, PARAMETERS)
         return self.wikipedia_return
 
-    def gmap_comm(self, input_user):
+    def gmap_comm(self, latitude, longitude):
         """google maps return a map about user input"""
-        PARAMETERS = self.communication.generate_parameters_gmaps(input_user)
+        PARAMETERS = self.communication.generate_parameters_gmaps(latitude, longitude)
         self.googlemap_return = self.communication.get_map(maps_url, PARAMETERS)
         return self.googlemap_return
