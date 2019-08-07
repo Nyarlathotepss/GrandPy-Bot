3747d7e552b2431966521c714443f0e37e31bc60
Nyarlathotepss <rcp.nmillet@gmail.com>
2019-08-07 10:17:56 +0200

    test has been improved : conftest.py has been created with fixture test_modifyuserinput is OK test_api NOK >>> restart all from scratch

--- tests/test_modifyuserinput.py
+++ tests/test_modifyuserinput.py
@@ -1,11 +1,26 @@
 from modifyuserinput import ModifyUserInput
-
-a = ModifyUserInput()
-b = ApiParameters()
-a.user_input = "bonjour marseille"
+from constant import grandpy_bot_question_empty, grandpy_bot_dont_understand
 
 
-def test_modification_process():
-    a.modification_process()
-    assert a.input_to_search == "marseille", "pb de saisie"
+def test_clean_text():
+    test = ModifyUserInput()
+    test.user_input = "bonjour as tu des infos sur  marseille"
+    test.modification_process(test.user_input)
+    assert test.input_to_search == "marseille"
 
+
+def test_clean_punctuation():
+    test = ModifyUserInput()
+    test.user_input = "notre-,dame-de-paris?!"
+    test.modification_process(test.user_input)
+    assert test.input_to_search == "notre dame paris"
+
+
+def test_sentence_empty(simulate_empty_input_user):
+    """simulate an empty user_input """
+    assert simulate_empty_input_user == grandpy_bot_question_empty
+
+
+def test_no_sense(simulate_nonsense_input_user):
+    """simulate an non sense user_input, for example the input user could be : blablablabla """
+    assert simulate_nonsense_input_user == grandpy_bot_dont_understand
